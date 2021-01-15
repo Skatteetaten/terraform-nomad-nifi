84880fea87c8500adefad8388979cabe1aa97d48
Sharma <you@example.com>
2021-01-15 13:02:23 +0100

    few changes

---
+++ example/standalone/README.md
@@ -0,0 +1,3 @@
+# Nifi Standalone example
+
+The current directory contains terraform related files that use the module in ../../. This example module spins up Nifi container in standalone mode.
--- example/standalone/main.tf
+++ example/standalone/main.tf
@@ -4,14 +4,12 @@ module "nifi" {
   # nomad
   nomad_datacenters = ["dc1"]
   nomad_namespace   = "default"
-  nomad_host_volume = "persistence"
 
   # nifi
   service_name    = "nifi"
   host            = "127.0.0.1"
   port            = 8182
   container_image = "apache/nifi:1.12.1"
-  use_host_volume = false
   use_canary      = false
   resource_proxy = {
     cpu    = 200
@@ -27,19 +25,18 @@ module "nifi" {
 }
 
 module "nifi_registry" {
-  source = "github.com/hannemariavister/terraform-nomad-nifiregistry?ref=0.1.0"
+  source = "github.com/hannemariavister/terraform-nomad-nifiregistry?ref=0.2.0"
 
   # nomad
   nomad_datacenters = ["dc1"]
   nomad_namespace   = "default"
-  nomad_host_volume = "persistence"
+
 
   # nifi
   service_name    = "nifi-registry"
   host            = "127.0.0.1"
   port            = 18080
   container_image = "apache/nifi-registry:0.8.0"
-  use_host_volume = false
   use_canary      = false
   resource = {
     cpu    = 500
