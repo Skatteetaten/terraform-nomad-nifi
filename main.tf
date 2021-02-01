090cdc29b9e40bfd8752cc29bc35c0ddfbfc4785
Cherradi <m77773@u1804i-0473.SKEAD.NO>
2021-02-01 10:51:50 +0100

    Lagt til variabel for vault_secret i main.tf

--- main.tf
+++ main.tf
@@ -5,21 +5,24 @@ locals {
 data "template_file" "nomad_job_nifi" {
   template = file("${path.module}/nomad/nifi.hcl")
   vars = {
-    datacenters     = local.datacenters
-    namespace       = var.nomad_namespace
-    image           = var.container_image
-    service_name    = var.service_name
-    host            = var.host
-    port            = var.port
-    cpu             = var.resource.cpu
-    memory          = var.resource.memory
-    cpu_proxy       = var.resource_proxy.cpu
-    memory_proxy    = var.resource_proxy.memory
-    use_canary      = var.use_canary
-
+    datacenters                = local.datacenters
+    namespace                  = var.nomad_namespace
+    image                      = var.container_image
+    service_name               = var.service_name
+    host                       = var.host
+    port                       = var.port
+    cpu                        = var.resource.cpu
+    memory                     = var.resource.memory
+    cpu_proxy                  = var.resource_proxy.cpu
+    memory_proxy               = var.resource_proxy.memory
+    use_canary                 = var.use_canary
+    vault_kv_policy_name       = var.vault_secret.vault_kv_policy_name
+    vault_kv_path              = var.vault_secret.vault_kv_path
+    vault_kv_field_user        = var.vault_secret.vault_kv_field_user
+    vault_kv_field_password    = var.vault_secret.vault_kv_field_password
     # nifi registry
-    registry_service_name = var.registry_service.service_name
-    registry_port         = var.registry_service.port
+    registry_service_name      = var.registry_service.service_name
+    registry_port              = var.registry_service.port
   }
 }
 
