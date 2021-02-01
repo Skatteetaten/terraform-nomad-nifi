fd0218e96e52c3feee44048cd0632c2767b35af8
Hanne Maria Vister <HanneMaria.Vister@skatteetaten.no>
2021-02-01 10:04:18 +0100

    remove nifi-reg from main.tf

--- example/standalone/main.tf
+++ example/standalone/main.tf
@@ -23,27 +23,3 @@ module "nifi" {
     host         = "100"
   }
 }
-
-//module "nifi_registry" {
-//  source = "github.com/hannemariavister/terraform-nomad-nifiregistry?ref=0.2.0"
-//
-//  # nomad
-//  nomad_datacenters = ["dc1"]
-//  nomad_namespace   = "default"
-//
-//
-//  # nifi
-//  service_name    = "nifi-registry"
-//  host            = "127.0.0.1"
-//  port            = 18080
-//  container_image = "apache/nifi-registry:0.8.0"
-//  use_canary      = false
-//  resource = {
-//    cpu    = 500
-//    memory = 1024
-//  }
-//  resource_proxy = {
-//    cpu    = 200
-//    memory = 128
-//  }
-//}
