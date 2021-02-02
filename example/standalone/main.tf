3a92d25b087f176de6993eb2f0e4af248fc5f576
Hanne Maria Vister <HanneMaria.Vister@skatteetaten.no>
2021-02-02 07:57:50 +0100

    resolved conflicts with master

--- example/standalone/main.tf
+++ example/standalone/main.tf
@@ -18,8 +18,32 @@ module "nifi" {
 
   #nifi registry
   registry_service = {
-    service_name = "nifi-registry"
-    port         = 18080
-    host         = "100"
+    service_name = module.nifi_registry.nifi_reg_service_name
+    port         = module.nifi_registry.nifi_reg_port
+    host         = module.nifi_registry.nifi_reg_host
+  }
+}
+
+module "nifi_registry" {
+  source = "github.com/hannemariavister/terraform-nomad-nifiregistry?ref=0.2.0"
+
+  # nomad
+  nomad_datacenters = ["dc1"]
+  nomad_namespace   = "default"
+
+
+  # nifi
+  service_name    = "nifi-registry"
+  host            = "127.0.0.1"
+  port            = 18080
+  container_image = "apache/nifi-registry:0.8.0"
+  use_canary      = false
+  resource = {
+    cpu    = 500
+    memory = 1024
+  }
+  resource_proxy = {
+    cpu    = 200
+    memory = 128
   }
 }
